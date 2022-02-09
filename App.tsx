b1c73ae3073bd2271947f71d647e2eaf2e3ffc75
Micheal Fletcher Jr <96737545+mfletcherjr@users.noreply.github.com>
2022-02-09 17:25:02 -0500

    developed login and reservation views second attempt

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet } from 'react-native';
+import LoginView from './components/login-view';
+import ReservationViewer from './components/reservation-view';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Drawer = createDrawerNavigator();
+
+let answer
+async function getData() {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@reservation')
+    answer = jsonValue != null ?JSON.parse(jsonValue):null;
+    console.log(answer);
+  } catch(e) {
+    console.log(e);
+  }
 }
+
+useEffect(()=>{getData()},[]);
+
+
+const [reservation, setReservation] = useState();
+
+console.log(reservation);
+
+
+
+return(
+  
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="LoginViewer">
+          <Drawer.Screen name="LoginViewer" component={LoginView} />
+          <Drawer.Screen name="ReservationViewer" component={ReservationViewer} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+  )
+}//end of App
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4b71bd',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- components/login-view.tsx
+++ components/login-view.tsx
@@ -1,46 +1,47 @@
 import { View, Text, TextInput, Button, Alert } from "react-native";
 import Reservation from "../dtos/reservation-dto";
+import { useState } from "react";
+import React from "react";
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// login page
-//sole focus is authentication based on valid username and password
-//http://52.146.51.210:3000/
 
-//make the reservation/component
 
-export default function LoginView(){ //prop needed? maybe passing in the 
 
-    const vacationer:Reservation ={
-        id: "009",
-        checkIn: 1000,
-        checkOut: 20000,
-        owner: "Joe Burreaux",
-        room: "SB MVP SUITE"
-    }
+
+export default function LoginView({navigation}){
 
     
 
-    let tempResID
-    
-   //develop function that reads the user input into a temp variable 
 
-    function grabResId(reservation:string){
-        tempResID = reservation;            
-    }
+    
+    const [resId, setResId] = useState("");
+  
+    
+    
 
     /** 
     if reservation exists, load reservation information
     to view: check-in, check-out, room
     else return reservation not found page to view
-
-
     */
-    function loadReservation(){
-        if (vacationer.id === tempResID) {
-            //create a component that will handle loading information
-            //of a valid reservation and it will be called here
+    async function loadReservation(){
+
+        try {
+            const response = await axios.get(`https://6894f2cd-bb6a-4979-8499-21dcd8a06fd1.mock.pstmn.io/reservations/${resId}`)
+            const grabbedRes:Reservation = response.data;
+            const jsonString = JSON.stringify(grabbedRes);
+            await AsyncStorage.setItem('@reservation', jsonString);
+            
+            
+            //setReservation(grabbedRes);
+           navigation.navigate('ReservationViewer', {jsonString});
 
 
-        } else {
+
+
+        } catch (error) {
+            console.log(error);
             Alert.alert("Reservation not found. Please check entry and try again.");
         }
 
@@ -48,13 +49,14 @@ export default function LoginView(){ //prop needed? maybe passing in the
 
    
     return(
-    <View>
-        <Text>Welcome to Paradise Villas log in</Text>
-        <TextInput onChangeText={grabResId} placeholder="Reservation ID"/>
-        <Button onPress={loadReservation} title="Submit Reservation Id" />
-
-
-    </View>)
+    
+        <View>
+            <Text>Welcome to Paradise Villas log in</Text>
+            <TextInput onChangeText={setResId} placeholder="Reservation ID"/>
+            <Button onPress={loadReservation} title="Submit Reservation Id" />
+        </View>
+    
+    )
 
 
 }
